7c480ac2012dfe0474aa006170e308baaad62862
darcectech <darylcecile@gmail.com>
2016-10-10 01:49:50 +0100

    updated version info

--- hydrogen3_download/IDE/updateManager.js
+++ hydrogen3_download/IDE/updateManager.js
@@ -2,3 +2,12 @@
 
 eventManager.triggerEvent('updating','checking','Checking server for latest updates...');
 
+console.log("u");
+
+// $.getJSON( "https://hydrogenEditor.github.io/version.json", function( data ) { 
+// 	console.log(data) 
+// });
+
+var s = $( "<p></p>" ).load("https://hydrogenEditor.github.io/version.json",function(){
+	console.log( $(this).html().version  )
+});
